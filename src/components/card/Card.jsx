f1aefc1fc534129110ba6304113b44b69dc28f11
Joseph VTX <1395609@senati.pe>
2022-10-31 20:08:28 -0500

    fix width

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,6 +1,6 @@
 export const Card = () => {
   return (
-    <div className='w-[22rem]  bg-white h-[40rem] lg:h-[28rem] lg:w-max rounded-xl  font-montserrat flex flex-col overflow-hidden lg:flex-row'>
+    <div className='w-[22rem]  bg-white h-[40rem] lg:h-[28rem] lg:w-[38rem] rounded-xl  font-montserrat flex flex-col overflow-hidden lg:flex-row'>
         
         <img src="../image-product-mobile.jpg" className='lg:hidden' alt  />
         <img src="../image-product-desktop.jpg" className='lg:block hidden' alt  />
@@ -9,7 +9,7 @@ export const Card = () => {
             
             <p className='tracking-[.3rem] text-[hsl(228,12%,48%)]'>PERFUME</p>
 
-            <h4  className='text-[2rem] font-bold font-fraunces leading-8 lg:pr-20'>Grabielle Essence Eau De Parfum</h4>
+            <h4  className='text-[2rem] font-bold font-fraunces leading-8 lg:pr-[4.75rem]'>Grabielle Essence Eau De Parfum</h4>
             
 
             <p className='text-[0.9375rem] text-[hsl(228,12%,48%)]'>A floral, solar and voluptous <br /> iterpretation composed by Oliver <br /> Polge, Perfumer-Creator for the House of CHANEL.</p>
@@ -20,7 +20,7 @@ export const Card = () => {
                 <p className='line-through text-[hsl(228,12%,48%)]'>$169.99</p>
             </div>
             
-            <button className='w-full bg-[hsl(158,36%,37%)] py-4 rounded-[0.625rem] text-white flex justify-center gap-2 items-center'> <img src="../icon-cart.svg" aria-hidden='true' alt /> Add to Cart</button>
+            <button className='w-full bg-[hsl(158,36%,37%)] py-4 rounded-[0.625rem] text-white flex justify-center gap-2 items-center'> <img src="/public/icon-cart.svg" aria-hidden='true' alt /> Add to Cart</button>
             
         </div>
     </div>
